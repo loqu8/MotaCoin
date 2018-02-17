b5418396c6368ea4484d8fc8a42b4a5276f9880e
Tim Uy <tim@loqu8.com>
2018-02-17 01:35:33 -0800

    oops, port should be different than defaultport

--- src/protocol.h
+++ src/protocol.h
@@ -19,7 +19,7 @@
 extern bool fTestNet;
 static inline unsigned short GetDefaultPort(const bool testnet = fTestNet)
 {
-    return testnet ? 23521 : 17421;
+    return testnet ? 23520 : 17420;
 }
 
 
